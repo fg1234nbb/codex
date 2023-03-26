ee1b7f487266a33b6027bf9287febed33f801e10
EricPai <47503110+fg1234nbb@users.noreply.github.com>
2023-03-26 10:55:47 -0500

    update server url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
 
 
-    const response = await fetch('http://localhost:3001', {
+    const response = await fetch('https://codex-render-vercel.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
